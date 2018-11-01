7a2795fa1bf765ac7e56a26083afe46251466073
Lucas Bustamante <lucasfbustamante@gmail.com>
2018-11-01 17:16:59 -0300

    Updated tests readme

--- mww/tests/readme.php
+++ mww/tests/readme.php
@@ -1,12 +1,19 @@
 <?php
 /**
- * To get started with tests, I recommend wp-browser.
- * WP-Browser is a set of extensions for Codeception, which in turn, is a wrapper around PHP Unit.
- * In my opinion, it's the most complete test suite for WordPress to this date.
+ * Modern WordPress Website uses lucatume/wp-browser for testing.
  *
- * Run this in the mu-plugins/mww folder:
- * composer require --dev lucatume/wp-browser
+ * lucatume/wp-browser is a set of extensions for Codeception, with a lot of useful methods for testing WordPress
+ * It can run acceptance, functional, integration and unit tests.
  *
- * Read installation instructions here:
+ * Codeception is built on top of PHPUnit, so in fact, we have this as testing framework:
+ * PHPUnit + Codeception + lucatume/wp-browser
+ *
+ * To learn how to test, you should learn PHPUnit, then Codeception, then lucatume/wp-browser,
+ * then everything will make sense, since one just extends the other.
+ *
+ * To run your tests edit mu-plugins/mww/.env.example and rename it to .env
+ * Then, run "codecept run functional"
+ *
+ * To learn how to write your own tests, see lucatume/wp-browser documentation:
  * https://github.com/lucatume/wp-browser
  */
