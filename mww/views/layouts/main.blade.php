ba2408115a979959ac3b7cef38759744e462b399
Andrew Rhyand <andrew.rhyand@gmail.com>
2019-10-13 09:51:34 -0700

    Replace core view layout with Blade structure

---
+++ mww/views/layouts/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html {{ language_attributes() }}>
+
+    <head>
+        <meta charset="{{ bloginfo('charset') }}">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        {!! wp_head() !!}
+    </head>
+
+    <body {{ body_class() }}>
+
+        <main>
+            @yield('content')
+        </main>
+
+        {!! wp_footer() !!}
+    </body>
+
+</html>
---
+++ mww/views/pages/404.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.main')
+
+@section('content')
+    <h1>Modern WordPress Website (MWW) is working!</h1>
+    <p>You are in: 404 - Route not found</p>
+@endsection
