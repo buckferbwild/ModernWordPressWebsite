1b9d190e97098c9138ff3365d0d37e44db83b95c
Lucas B <lucasfbustamante@gmail.com>
2019-12-11 19:08:00 -0300

    Trailingslash path constants

--- mww.php
+++ mww.php
@@ -12,24 +12,23 @@
 use App\Bootstrap;
 use MWW\DI\Container;
 
-define( 'MWW_FOLDER', '/mww' );
-define( 'MWW_PATH', __DIR__ . MWW_FOLDER );
-define( 'MWW_START', microtime( true ) );
-define( 'MWW_URL', plugin_dir_url( __FILE__ ) . MWW_FOLDER );
+define( 'MWW_FOLDER', trailingslashit( '/mww' ) );
+define( 'MWW_URL', trailingslashit( plugin_dir_url( __FILE__ ) . MWW_FOLDER ) );
+define( 'MWW_PATH', trailingslashit( __DIR__ . MWW_FOLDER ) );
 
-/** Registers Composer Autoloader */
-if ( file_exists( MWW_PATH . '/vendor/autoload.php' ) ) {
-	require_once( MWW_PATH . '/vendor/autoload.php' );
+/** Composer Autoloader */
+if ( file_exists( MWW_PATH . 'vendor/autoload.php' ) ) {
+	require_once( MWW_PATH . 'vendor/autoload.php' );
 } else {
 	throw new Exception( 'You need to run "composer update" in the following folder "' . MWW_PATH . '" to get started.' );
 }
 
 /** Registers helper functions */
-require_once( MWW_PATH . '/framework/Support/helpers.php' );
+require_once( MWW_PATH . 'framework/Support/helpers.php' );
 
 /** Builds the Dependency Injection Container and registers Service Providers */
-Container::registerBindings( MWW_PATH . '/framework/bindings.php' );
-Container::registerBindings( MWW_PATH . '/app/bindings.php' );
+Container::registerBindings( MWW_PATH . 'framework/bindings.php' );
+Container::registerBindings( MWW_PATH . 'app/bindings.php' );
 
 /** Yahoo! */
 Container::make( Bootstrap::class )->run();
