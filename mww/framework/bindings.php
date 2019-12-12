f6de341af756f850d9d680e480fa52b1a02386a3
Lucas B <lucasfbustamante@gmail.com>
2019-12-12 02:02:33 -0300

    Removing unused bindings file

--- mww/framework/bindings.php
+++
@@ -1,9 +0,0 @@
-<?php
-/** Bindings for dependency injection container */
-
-use MWW\DI\Container;
-use MWW\Routing\Condition;
-use MWW\Templates\Template;
-
-Container::singleton( Template::class, Template::class );
-Container::bind( Condition::class, Condition::class );
